883550d3430ec32a1fde93795067419f591ac421
ChattersonR <robertchatterson1@gmail.com>
2018-12-09 19:07:37 -0500

    Added import functionality. Refactored to genarify common tasks.

--- photos.py
+++ photos.py
@@ -18,6 +18,7 @@ import logging
 import sys
 
 from PIL import Image
+from shutil import copyfile
 import exifread
 import os
 import argparse
@@ -32,35 +33,21 @@ SIDECAR_NAME = ['.xmp']
 logger = logging.getLogger("camera-roll-utils")
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
-def walklevel(some_dir, level=1):
-    some_dir = some_dir.rstrip(os.path.sep)
-    assert os.path.isdir(some_dir)
-    num_sep = some_dir.count(os.path.sep)
-    for root, dirs, files in os.walk(os.path.normpath(some_dir)):
-        yield root, dirs, files
-        num_sep_this = root.count(os.path.sep)
-        if num_sep + level <= num_sep_this:
-            del dirs[:]
-
-def importFilesFromCard(importPath, dryRun=False):
-    print("todo")
-    #import files from card
-
-def organizeInPlace(cameraRollDirectory, dryRun=False):
+def preProcessInputDir(importPath):
     existingDirectories = []
     unsortedRaw = {}
     sidecars = {}
     unsortedJpg = {}
-    for root, subdirs, files in os.walk(os.path.normpath(cameraRollDirectory)):
-        if root == cameraRollDirectory:
+    for root, subdirs, files in os.walk(os.path.normpath(importPath)):
+        if root == importPath:
             for _dir in subdirs:
                 try:
                     datetime.datetime.strptime(_dir, DATE_FILE_FORMAT)
                     existingDirectories.append(_dir)
                 except:
-                  pass
+                    pass
         elif not any(substring in root for substring in existingDirectories):
-            print("Skippign directory: {}", root)
+            print("Skipping directory: {}", root)
             continue
 
         for _file in files:
@@ -82,9 +69,19 @@ def organizeInPlace(cameraRollDirectory, dryRun=False):
             if workingDict.get(key) is None:
                 workingDict[key] = []
             workingDict[key].append(os.path.join(root, _file))
+    return existingDirectories, unsortedRaw, sidecars, unsortedJpg
+
+def importFilesFromCard(directories, dryRun=False):
+    outputSortedFiles(directories[1], preProcessInputDir(directories[0]), copyfile, dryRun=dryRun)
+
+def organizeInPlace(cameraRollDirectory, dryRun=False):
+    outputSortedFiles(cameraRollDirectory, preProcessInputDir(cameraRollDirectory), os.rename, dryRun=dryRun)
+
+def outputSortedFiles(outputDirectory, preProcessResults, transferCommand, dryRun=False):
+    existingDirectories, unsortedRaw, sidecars, unsortedJpg = preProcessResults
 
     for _key, _value in unsortedRaw.items():
-        _path = os.path.join(cameraRollDirectory, _key)
+        _path = os.path.join(outputDirectory, _key)
         if _key not in existingDirectories:
             try:
                 logger.debug("mkdir {}".format(_path))
@@ -98,23 +95,23 @@ def organizeInPlace(cameraRollDirectory, dryRun=False):
             basename = os.path.basename(_file)
             if os.path.join(_path, basename) != _file:
                 if not os.path.exists(os.path.join(_path, basename)):
-                    logger.debug("mv {} {}".format(_file, os.path.join(_path, basename)))
+                    logger.debug("{} {} {}".format(transferCommand.__str__(), _file, os.path.join(_path, basename)))
                     if not dryRun:
-                        os.rename(_file, os.path.join(_path, basename))
+                        transferCommand(_file, os.path.join(_path, basename))
                 else:
                     logger.error("ERROR: File already exists. From {} To {}".format(_file, os.path.join(_path, basename)))
             if sidecars.get(basename) is not None \
                     and os.path.join(_path, os.path.basename(sidecars[basename])) != sidecars[basename]:
                 sidecarPath = os.path.join(_path, os.path.basename(sidecars[basename]))
                 if not os.path.exists(sidecarPath):
-                    logger.debug("mv {} {}".format(sidecars[basename], sidecarPath))
+                    logger.debug("{} {} {}".format(transferCommand.__str__(), sidecars[basename], sidecarPath))
                     if not dryRun:
-                        os.rename(sidecars[basename], sidecarPath)
+                        transferCommand(sidecars[basename], sidecarPath)
                 else:
                     logger.error("ERROR: File already exists. From {} To {)".format(_file, sidecarPath))
 
     for _key, _value in unsortedJpg.items():
-        _path = os.path.join(cameraRollDirectory, _key, 'jpg')
+        _path = os.path.join(outputDirectory, _key, 'jpg')
         if not os.path.exists(_path):
             try:
                 logger.debug("mkdir {}".format(_path))
@@ -128,9 +125,9 @@ def organizeInPlace(cameraRollDirectory, dryRun=False):
             basename = os.path.basename(_file)
             if os.path.join(_path, basename) != _file:
                 if not os.path.exists(os.path.join(_path, basename)):
-                    logger.debug("mv {} {}".format(_file, os.path.join(_path, basename)))
+                    logger.debug("{} {} {}".format(transferCommand.__str__(), _file, os.path.join(_path, basename)))
                     if not dryRun:
-                        os.rename(_file, os.path.join(_path, basename))
+                        transferCommand(_file, os.path.join(_path, basename))
                 else:
                     logger.error("ERROR: File already exists. From {} To {}".format(_file, os.path.join(_path, basename)))
 
@@ -159,21 +156,21 @@ subparsers = parser.add_subparsers(help="Commands", dest="command")
 subparsers.required = True
 organizeParser = subparsers.add_parser("organize", help="Organize an existing Camera Roll directory to conform to the standard.")
 organizeParser.add_argument("directory",
-                          #type=argparse.FileType('r'),
+                          #type=argparse.FileType('rb'),
                           type=str,
                           )
 
 importParser = subparsers.add_parser("import", help="Import pictures from an external source to the camera roll.")
 importParser.add_argument("directory",
                           help="The directories. import <importDirectory> <outputDirectory>",
-                          #type=argparse.FileType('r'),
+                          #type=argparse.FileType('rb'),
                           type=str,
                           nargs=2
                           )
 
 cleanupParser = subparsers.add_parser("cleanup", help="Cleanup any JPG images in the Camera Roll that correspond with deleted raw files.")
 cleanupParser.add_argument("directory",
-                          #type=argparse.FileType('r'),
+                          #type=argparse.FileType('rb'),
                           type=str,
                           )
 
